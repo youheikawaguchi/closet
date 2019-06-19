27aa9d7f91f3bd4954c3ace38023b630ee0e0aa6
大坪一輝 <2019.otsubo@gmail.com>
2019-06-19 16:24:33 +0900

    アカウント作成ver1.0

---
+++ closet/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.model.User;
+import com.example.service.UserService;
+
+@Controller
+public class UserController {
+	
+	UserService userService;
+	
+	@ModelAttribute
+	public User setupForm() {
+		return new User();
+		}
+	
+	@GetMapping(value="users/createacc")
+	ModelAndView userForm(ModelAndView mav) {
+	
+		mav.setViewName("users/createacc");
+		return mav;
+	}
+
+	@RequestMapping(value = "user", method = RequestMethod.POST)
+	String create(@Validated User form, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "users/createacc";
+		}
+		User user =new User();
+		user.setUserId(form.getUserId());
+		userService.create(user, form.getPassword());
+		return "redirect:/users/profile";
+	}
+	
+	@RequestMapping(value = "users/profile", method = RequestMethod.GET)
+	String createFinish() {
+		return "users/profile";
+	}
+}
