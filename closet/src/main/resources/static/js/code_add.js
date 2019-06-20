453e4a5aa74eebce3f232016ff61acbc9201ac0b
a_kawase <aprikoze1203@gmail.com>
2019-06-20 10:24:31 +0900

    修正

--- closet/src/main/resources/static/js/code_add.js
+++ closet/src/main/resources/static/js/code_add.js
@@ -1,3 +1,10 @@
 function c_Select(){
-	
+	if(doucument.getElementbyId('select1')){
+		var id = doucument.getElementbyId('select1').value;
+	    document.getElementById('code').style.display = "none";
+	    document.getElementById('calender').style.display = "none";
+	    if(id == '1'){
+	        document.getElementById('1').style.display = "";
+	    }
+	}
 }
