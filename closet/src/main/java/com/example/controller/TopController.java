9ccd6c8984ecc5bc9e011b6b5804ab855516520c
mai_murakami <mai.murakami.tcmobile@gmail.com>
2019-06-20 19:31:59 +0900

    topにスライドショーとお天気を追加

--- closet/src/main/java/com/example/controller/TopController.java
+++ closet/src/main/java/com/example/controller/TopController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,8 +18,8 @@ public class TopController {
 	
 	@GetMapping("/top")
 	public ModelAndView top(ModelAndView mav) {
-		List<Item> items = topService.getAllItems();
-		mav.addObject("items", items);
+		//List<Item> items = topService.getAllItems();
+		//mav.addObject("items", items);
 		mav.setViewName("top/top");
 		return mav;
 	}
