fcccaf9c3fff0fd8bda1cf4bd8c14d08065062a4
大坪一輝 <2019.otsubo@gmail.com>
2019-06-20 18:03:22 +0900

    ユーザー登録ver1.0

--- closet/src/main/java/com/example/service/UserService.java
+++ closet/src/main/java/com/example/service/UserService.java
@@ -3,19 +3,26 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.model.User;
 import com.example.repository.UserRepository;
 @Service
+@Transactional
 public class UserService {
 	@Autowired
 	UserRepository userRepository;
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	
-	public User create(User user, String rawPassword) {
-		String encodedPassword = passwordEncoder.encode(rawPassword);
-		user.setPassword(encodedPassword);
-		return userRepository.save(user);
+	public User createUser(User user) {
+//		String encodedPassword = passwordEncoder.encode(rawPassword);
+//		user.setPassword(encodedPassword);
+		
+//		user.setUserId(user.userId);
+//		user.setPassword(password);
+		
+		return userRepository.saveAndFlush(user);
 	}
+	
 }
