e55e363cdbf539c734981be14693f715090e310e
a_kawase <aprikoze1203@gmail.com>
2019-06-21 14:29:29 +0900

    プロフィール登録/カレンダー登録の画面表示の設定

--- closet/src/main/resources/static/js/code_add.js
+++ closet/src/main/resources/static/js/code_add.js
@@ -1,10 +1,17 @@
-function c_Select(){
-	if(doucument.getElementbyId('select1')){
-		var id = doucument.getElementbyId('select1').value;
-	    document.getElementById('code').style.display = "none";
-	    document.getElementById('calender').style.display = "none";
-	    if(id == '1'){
-	        document.getElementById('1').style.display = "";
-	    }
-	}
+window.onload = function c_Code(){
+    var selected = document.getElementById('select1').value;
+    document.getElementById('c_code').style.display = "none";
 }
+    
+    function c_Code(){
+    var selected = document.getElementById('select1').value;
+    document.getElementById('c_code').style.display = "none";
+    if(selected == 1){
+        document.getElementById('calender').style.display = "none";
+        document.getElementById('c_code').style.display = "block";
+        //element.classList.remove("c_code");
+    }else if(selected == 2){
+        document.getElementById('c_code').style.display = "none";
+        document.getElementById('calender').style.display = "block";
+    }
+}; 
