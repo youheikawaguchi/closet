54e33f89f94def4eeb45af181b3aff263beb5544
n.mizoguchi <n.mizoguchi@tcmobile.jp>
2019-06-26 22:20:24 +0900

    カレンダー処理修正

--- closet/src/main/resources/static/js/calendar/calendar.js
+++ closet/src/main/resources/static/js/calendar/calendar.js
@@ -1,6 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   var calendarEl = document.getElementById('calendar');
-//   var calendar = new FullCalendar.Calendar(calendarEl, {
 $(document).ready(function () {
   $('#calendar').fullCalendar({
     plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
@@ -14,66 +11,34 @@ $(document).ready(function () {
     selectLongPressDelay: 1,
 
     dayClick: function(date, allDay, jsEvent, view) {
-//      $('#calendar').fullCalendar('addEventSource', [{
-//        title: 'aaa',
-//        start: date,
-//        allDay: true
-//      }]);
-      var select = $(".fc-highlight-skeleton").parent();
-      var h = select.height();
-      var i = $(".fc-row").index(select) - 1;
-      $("#calendar .fc-scroller").height(h);
-      $("#calendar .fc-scroller").animate({scrollTop:h*i});
-      $("#item p").text(date.format());
+    	var d = new Date(date);
+		var select = $(".fc-highlight-skeleton").parent();
+		var h = select.height();
+		var i = $(".fc-row").index(select) - 1;
+		$("#calendar .fc-scroller").height(h);
+		$("#calendar .fc-scroller").animate({scrollTop:h*i});
+		$("#item #date").text(date.format());
       
-      // イベントの有無を判定
-//      date = new Date(date);
-//      var date2 = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1);
-//      var events = $("#calendar").fullCalendar('clientEvents',(function(event) {
-//        console.log(event.start >= date && event.start < date2);
-//      }));
-//      if(events==true){
-//    	  $this.eventClick();
-//      }
-
-    },
-    eventClick: function(calEvent, jsEvent, view) {
-    	
-    	//スライドイン
-        var select = $(".fc-highlight-skeleton").parent();
-        var h = select.height();
-        var i = $(".fc-row").index(select) - 1;
-        $("#calendar .fc-scroller").height(h);
-        $("#calendar .fc-scroller").animate({scrollTop:h*i});
-
-    	var date = new Date(calEvent['start']['_i']);
-    	var date_str = 'YYYY-MM-DD';
-    	date_str = date_str.replace(/YYYY/g, date.getFullYear());
-    	date_str = date_str.replace(/MM/g, date.getMonth());
-    	date_str = date_str.replace(/DD/g, date.getDate());
-    	  
-    	  
-        $("#item p").text(date_str);
-        
-    	$('#cordinate-title').text(calEvent.title);
-    	
-    	$.ajax({
+		$.ajax({
 			url: "/calendar/getdatecoorde",
 			dataType: "json",
 			type:"GET",
 			data: {
-				"date": date,
+				"date": d,
 				"userid" : 1
 			},
-		success: function(cal) {
-			console.log(cal.metPerson);
-			console.log(cal.coordinate);
-			
-	    },
-	    error: function(data) {
-	        alert("コーデ情報が取得できませんでした。");
-	    }
-	  });
+			success: function(cal) {
+				$('#cordinate-title').text(cal.event);
+				console.log(cal.metPerson);
+				console.log(cal.coordinate);
+				
+		    },
+		    error: function(data) {
+		    	$('#cordinate-title').text(date.format("YYYY-MM-DD"));
+		    }
+		});
+	      
+
     },
     viewRender: function(view) {
 	  var date = $("#calendar").fullCalendar("getDate");
